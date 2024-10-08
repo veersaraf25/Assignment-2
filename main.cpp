f28ef146f142a36ac0c6a8254236811c22a199e8
Aaryan Nagpal <aaryannagpal@gmail.com>
2024-10-09 01:23:37 +0530

    main template

---
+++ main.cpp
@@ -0,0 +1,25 @@
+#include "HashTable.cpp"
+
+int main() {
+    int initialSize = 7; 
+    HashTable ht(initialSize);
+
+    // Example test case
+    ht.insert(1);
+    ht.printTable();
+    ht.insert(6);
+    ht.printTable();
+    ht.insert(15);
+    ht.printTable(); 
+    ht.insert(25);
+    ht.printTable();
+    ht.remove(15);
+    ht.printTable();
+    ht.insert(29);  
+    ht.printTable(); 
+
+    int find = ht.search(22);
+    std::cout << "Found at:" << find << std::endl;
+
+    return 0;
+}
